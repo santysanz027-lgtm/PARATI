19bc4a5b5a7217c7ccf0afa387dc850110cce976
santysanz027-lgtm <santysanz027@gmail.com>
2025-12-06 00:22:36 -0500

    Update button text and improve mobile loader

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var doc=document,flower=doc.querySelector('.flower'),petalPartMarkup='<div class
   if (mainContent) mainContent.style.display = '';
   // Efecto typing en el botón al inicio
   var startBtn = document.getElementById('start-btn');
-  var btnText = 'Presioname';
+  var btnText = 'Tocame 😏';
   startBtn.textContent = '';
   startBtn.disabled = true;
   let iBtn = 0;
@@ -252,5 +252,6 @@ var doc=document,flower=doc.querySelector('.flower'),petalPartMarkup='<div class
     }
   });
 });
+
 
 
